aa7282d3a7483cd1ec180939dbcb8de4e7ef2882
prafulgabhane <prafulgabhane786@gmail.com>
2022-10-12 20:58:00 +0530

    Adding code to the UC-3 branch

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,13 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AddressBook
 {
     public class Program
     {
-        public static void Main(String[] args)
+        static void Main(string[] args)
         {
-            AddressBookClass add = new AddressBookClass();
-            add.AddContact();
+            //UC-3 Ability to edit their existing contact details using their names
+            Console.WriteLine("Welcome to Address Book");
+
+
+            int choice = 0;
+            while (choice != 2)
+            {
+                Console.WriteLine("1.Perform Operations \n2.Exit");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice != 2)
+                    AddressBook.AddPerson();
+            }
+            AddressBook.ListPeople();
+            Console.WriteLine("Program Ends: Address Book: Press Enter");
+            Console.ReadLine();
+
         }
     }
 }
